3f9ea986b346a9cad01b97893e696930b8f0f517
Vitalik <frantsv2004@gmail.com>
2024-12-04 18:56:56 +0100

    feat: add function to clean branch

--- pkg/github/branches.go
+++ pkg/github/branches.go
@@ -1,6 +1,9 @@
 package github
 
-import "fmt"
+import (
+	"dashinette/internals/logger"
+	"fmt"
+)
 
 // Checks if a branch exists in the repository.
 func isBranchExist(repoPath string, branch string) bool { // todo
@@ -30,5 +33,24 @@ func SwitchBranch(repoPath string, branch string) (err error) {
 	if err != nil {
 		return fmt.Errorf("failed to push branch: %w", err)
 	}
+	return nil
+}
+
+// Creates an empty branch in the repository.
+func SwitchEmptyBranch(repoPath string, branch string) (err error) {
+	if isBranchExist(repoPath, branch) {
+		if err = executeCommand(repoPath, "git", "switch", branch); err != nil {
+			return fmt.Errorf("failed to switch branch: %w", err)
+		}
+	} else {
+		if err = executeCommand(repoPath, "git", "checkout", "--orphan", branch); err != nil {
+			return fmt.Errorf("failed to create branch: %w", err)
+		}
+	}
+
+	logger.Info.Printf("Cleaning branch %s", branch)
+    if err := executeCommand(repoPath, "git", "rm", "-rf", "."); err != nil {
+        return fmt.Errorf("failed to remove tracked files in branch %s: %w", branch, err)
+    }
 	return nil
 }
