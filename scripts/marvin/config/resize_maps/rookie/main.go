b86bbea8ef167110b4f325144d9b380e46f9355e
Vitalik <frantsv2004@gmail.com>
2024-12-04 15:28:19 +0100

    feat: add resizing script for rookie league contest

--- scripts/marvin/config/resize_maps/rookie/main.go
+++ scripts/marvin/config/resize_maps/rookie/main.go
@@ -9,12 +9,41 @@ import (
 )
 
 var (
-	scenario                    int
-	rookie_noise_generator      string = "scripts/marvin/maps/generators/rookie_noise/"
-	rookie_image_generator      string = "scripts/marvin/maps/generators/rookie_image/"
-	rookie_final_map_folder_rel string = "../../../../../dashes/marvin/maps/final_rookie/"
-	rookie_final_img_folder_rel string = "../../../../../dashes/marvin/maps/final_rookie_images/"
+	scenario     int
+	resultFolder string = "../../../../../dashes/marvin/maps/final_rookie"
+	imageFolder  string = "../../../../../dashes/marvin/maps/final_rookie_images"
 )
+
+var (
+	imageGenerationScript = []string{"scripts/marvin/maps/generators/rookie_image", "go", "run", "main.go"}
+	noiseGenerationScript = []string{"scripts/marvin/maps/generators/rookie_noise", "go", "run", "main.go"}
+)
+
+var parametrs = [][]string{
+	{"50", "50", "1:9", "0:0", "-1:-1"},
+	{"100", "100", "1:9", "-1:-1", "0:0"},
+	{"150", "150", "1:9", "0:0", "-1:-1"},
+	{"100", "100", "1:3", "-1:0", "0:-1"},
+	{"100", "100", "5:9", "0:0", "-1:-1"},
+	{"100", "100", "8:9", "9:-10", "-10:9"},
+	{"300", "300", imagePath(6), "t", "0:-1", "-1:0"},
+	{"250", "250", imagePath(7), "f", "2:-3", "-5:2"},
+	{"400", "400", imagePath(8), "f", "-5:5", "5:-5"},
+	{"400", "400", imagePath(9), "f", "5:-5", "-5:5"},
+	{"400", "600", imagePath(10), "f", "-5:5", "5:-4"},
+	{"400", "400", imagePath(11), "f", "25:25", "-25:-25"},
+	{"400", "200", imagePath(12), "f", "-5:-5", "20:-80"},
+	{"300", "450", imagePath(13), "f", "20:-105", "-5:-20"},
+	{"400", "300", imagePath(14), "f", "-23:10", "10:50"},
+	{"400", "400", imagePath(15), "t", "30:-30", "-23:170"},
+	{"450", "450", imagePath(16), "t", "10:10", "-13:-13"},
+	{"500", "500", imagePath(17), "f", "10:10", "-13:-13"},
+	{"500", "900", imagePath(18), "f", "10:200", "-13:-11"},
+	{"500", "900", imagePath(19), "t", "10:-10", "-43:413"},
+	{"500", "900", imagePath(20), "t", "70:110", "-33:-213"},
+	{"500", "900", imagePath(21), "t", "-20:15", "-213:313"},
+	{"500", "900", imagePath(22), "t", "0:-1", "-1:0"},
+}
 
 func executeCommand(dir, command string, args ...string) error {
 	cmd := exec.Command(command, args...)
@@ -26,48 +55,31 @@ func executeCommand(dir, command string, args ...string) error {
 	return cmd.Run()
 }
 
+func imagePath(i int) string {
+	return fmt.Sprintf("%v/%d.png", imageFolder, i)
+}
+
+func getDestination(i int) string {
+	return fmt.Sprintf("%s/%d.txt", resultFolder, i)
+}
+
 func selectScenario() [][]string {
 	var scripts [][]string
-	switch scenario {
-	case 0:
-		scripts = [][]string{
-			{"30:30", "1:9", "0:0", "29:29"},
-			{"60:60", "1:9", "59:59", "0:0"},
-			{"90:90", "1:9", "0:0", "89:89"},
-			{"60:60", "1:3", "59:0", "0:59"},
-			{"60:60", "5:9", "0:0", "59:59"},
-			{"60:60", "8:9", "9:49", "49:9"},
-			{"180:180", rookie_final_img_folder_rel + "6.png", "t", "0:179", "179:0"},
-		}
-	case 1:
-		scripts = [][]string{
-			{"40:40", "1:9", "0:0", "39:39"},
-			{"80:80", "1:9", "79:79", "0:0"},
-			{"120:120", "1:9", "0:0", "119:119"},
-			{"80:80", "1:3", "79:0", "0:79"},
-			{"80:80", "5:9", "0:0", "79:79"},
-			{"80:80", "8:9", "9:69", "69:9"},
-			{"240:240", rookie_final_img_folder_rel + "6.png", "t", "0:239", "239:0"},
-		}
-	default:
-		scripts = [][]string{
-			{"50:50", "1:9", "0:0", "49:49"},
-			{"100:100", "1:9", "99:99", "0:0"},
-			{"150:150", "1:9", "0:0", "149:149"},
-			{"100:100", "1:3", "99:0", "0:99"},
-			{"100:100", "5:9", "0:0", "99:99"},
-			{"100:100", "8:9", "9:89", "89:9"},
-			{"300:300", rookie_final_img_folder_rel + "6.png", "t", "0:299", "299:0"},
-		}
+
+	for _, param := range parametrs {
+		rows, _ := strconv.Atoi(param[0])
+		cols, _ := strconv.Atoi(param[1])
+		rows = rows * scenario / 100
+		size := []string{fmt.Sprintf("%d:%d", rows, cols)}
+		scripts = append(scripts, append(size, param[2:]...))
 	}
 	for i := range scripts {
 		if i < 6 {
-			scripts[i] = append([]string{rookie_noise_generator, "go", "run", "main.go"}, scripts[i]...)
-			scripts[i] = append(scripts[i], fmt.Sprintf("%s%d.txt", rookie_final_map_folder_rel, i))
+			scripts[i] = append(noiseGenerationScript, scripts[i]...)
 		} else {
-			scripts[i] = append([]string{rookie_image_generator, "go", "run", "main.go"}, scripts[i]...)
-			scripts[i] = append(scripts[i], fmt.Sprintf("%s%d.txt", rookie_final_map_folder_rel, i))
+			scripts[i] = append(imageGenerationScript, scripts[i]...)
 		}
+		scripts[i] = append(scripts[i], getDestination(i))
 	}
 	return scripts
 }
@@ -85,7 +97,7 @@ func main() {
 func init() {
 	fmt.Println("Resize maps")
 	if len(os.Args) != 2 {
-		log.Fatal("usage: ./main [scenario 0-3]")
+		log.Fatal("usage: ./main [scenario 0/1/2/3]")
 	}
 
 	var err error
@@ -96,5 +108,16 @@ func init() {
 
 	if scenario < 0 || scenario > 3 {
 		log.Fatal("scenario must be between 0 and 3")
+	}
+
+	switch scenario {
+	case 0:
+		scenario = 60
+	case 1:
+		scenario = 80
+	case 2:
+		scenario = 100
+	case 3:
+		scenario = 120
 	}
 }
